a227e952ed7f24b2818fb0ee6750f6bc47c6fe0e
Michael Statt <michael.statt@modelyst.io>
2021-11-10 07:34:31 -0500

    removed all dynamic versioning as it is too buggy for now

--- src/dbgen/__init__.py
+++ src/dbgen/__init__.py
@@ -18,7 +18,7 @@ __author__ = "Michael Statt"
 __email__ = "michael.statt@modelyst.io"
 __maintainer__ = "Michael Statt"
 __maintainer_email__ = "michael.statt@modelyst.io"
-__version__ = "0.0.0"  # this is placeholder automatically set by poetry-dynamic-versioning
+__version__ = "0.5.8"
 
 
 from dbgen.core.args import Const
