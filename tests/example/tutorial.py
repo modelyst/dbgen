d94745af54669c6a0183661aae89c3daa9194c6a
Michael Statt <michael.statt@modelyst.io>
2021-11-10 13:20:54 -0500

    updated tests

--- tests/example/tutorial.py
+++ tests/example/tutorial.py
@@ -1,7 +1,24 @@
-from dbgen import Entity, Extract, Model, Generator, Env, Import, transform
+#   Copyright 2021 Modelyst LLC
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
-import numpy
 import os
+from typing import List
+
+import numpy as np
+
+from dbgen import Entity, Env, Extract, Generator, Import, Model
+from dbgen.core.decorators import transform
 
 
 class JVCurve(Entity, table=True):
@@ -13,8 +30,10 @@ class JVCurve(Entity, table=True):
 
 class LocalCSVExtract(Extract):
     data_dir: str
+    outputs: List[str] = ['file_path']
+    _file_paths = None
 
-    def setup(self):
+    def setup(self, **_):
         self._file_paths = [
             os.path.join(self.data_dir, fname)
             for fname in os.listdir(self.data_dir)
@@ -30,28 +49,28 @@ class LocalCSVExtract(Extract):
         return len(self._file_paths)
 
 
-def make_model():
+@transform(env=Env(Import('numpy', lib_alias='np')), outputs=['voc', 'jsc'])
+def parse_jv_csv(file_path: str) -> tuple[float, float]:
+    jv_arr = np.genfromtxt(file_path, delimiter=',', skip_header=1, dtype=float)
+    nrows, *_ = np.shape(jv_arr)
+    for row in range(nrows):
+        if jv_arr[row][0] > 0:
+            jsc = jv_arr[row][1]
+            break
 
+    for row in range(nrows):
+        if jv_arr[row][1] > 0:
+            voc = jv_arr[row][0]
+            break
+
+    return voc, jsc
+
+
+def make_model():
     with Model(name="example") as model:
         with Generator('load_jv_curves'):
-            extract = LocalCSVExtract(data_dir=os.environ['DATA_DIR'], outputs=['file_path'])
+            extract = LocalCSVExtract(data_dir=os.environ['DATA_DIR'])
             file_path = extract['file_path']
-
-            @transform(env=Env([Import('numpy')]), outputs=['voc', 'jsc'])
-            def parse_jv_csv(file_path: str) -> tuple[float, float]:
-                jv_arr = numpy.genfromtxt(file_path, delimiter=',', skip_header=1, dtype=float)
-                for row in range(numpy.shape[0]):
-                    if jv_arr[row][0] > 0:
-                        jsc = jv_arr[row][1]
-                        break
-
-                for row in range(numpy.shape[0]):
-                    if jv_arr[row][1] > 0:
-                        voc = jv_arr[row][0]
-                        break
-
-                return voc, jsc
-
             voc, jsc = parse_jv_csv(file_path).results()
 
             JVCurve.load(
