352b09ff85c1a1e740b4c6bbadbcb4b34edb07db
Brian Rohr <brohr12@gmail.com>
2022-03-02 13:17:02 -0800

    feat(examples): added the clean flag for the alice_bob_model

--- examples/alice_bob_lab/{{cookiecutter.repo_name}}/alice_bob_model/etl_steps/__init__.py
+++ examples/alice_bob_lab/{{cookiecutter.repo_name}}/alice_bob_model/etl_steps/__init__.py
@@ -1,3 +1,4 @@
+# Add etl_steps here
 from alice_bob_model.etl_steps.f_to_c import add_f_to_c
 from alice_bob_model.etl_steps.parse_measurements import add_temperature_etl_step
 from alice_bob_model.etl_steps.read_csv import add_io_etl_step
