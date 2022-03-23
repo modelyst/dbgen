eafb2e325117dc656d4f04b33d94fc83836cf217
Michael Statt <michael.statt@modelyst.io>
2022-03-23 17:28:54 -0400

    fix(common extractors): move extractors to providers and add tests and error messages

--- src/dbgen/providers/aws/extract.py
+++ src/dbgen/providers/aws/extract.py
@@ -12,4 +12,64 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-# TODO add s3 extractor
+import re
+from typing import Any
+from typing import Generator as GenType
+from typing import List, Optional, Tuple
+
+import boto3
+
+from dbgen import Extract
+
+
+class S3FileNameExtractor(Extract[str]):
+    bucket: str
+    profile: Optional[str]
+    prefix: str = ''
+    pattern: str = '.*'
+    outputs: List[str] = ['s3_key']
+
+    def extract(self) -> GenType[str, None, None]:
+        """Return the files in a directory on s3 bucket that match pattern."""
+        session = boto3.Session(profile_name=self.profile)
+        s3 = session.client("s3")
+        regex = re.compile(self.pattern, re.MULTILINE)
+
+        response = s3.list_objects_v2(Bucket=self.bucket, Prefix=self.prefix)
+        assert "Contents" in response, response
+        if not response or response["KeyCount"] == 0:
+            return
+
+        def process_keys(response: dict) -> Tuple[List[str], Any]:
+            return (
+                [content["Key"] for content in response["Contents"] if regex.search(content["Key"])],
+                response.get("NextContinuationToken"),
+            )
+
+        vals, token = process_keys(response)
+        yield from vals
+        count = 0
+        while token:
+            response = s3.list_objects_v2(Bucket=self.bucket, Prefix=self.prefix, ContinuationToken=token)
+            if not response["IsTruncated"]:
+                break
+
+            keys, token = process_keys(response)
+            yield from keys
+            count += response["KeyCount"]
+
+
+class S3FileExtractor(S3FileNameExtractor):
+    """Returns both the key and contents of the file on s3"""
+
+    outputs: List[str] = ['s3_key', 'contents']
+
+    def extract(self) -> GenType[Tuple[str, str], None, None]:  # type: ignore
+        session = boto3.Session(profile_name=self.profile)
+        s3 = session.resource("s3")
+        key_gen = super().extract()
+        bucket = s3.Bucket(self.bucket)
+        for key in key_gen:
+            obj = bucket.Object(key=key)
+            body = obj.get()["Body"].read().decode("utf-8")
+            yield key, body
--- src/dbgen/providers/common/__init__.py
+++ src/dbgen/providers/common/__init__.py
@@ -11,15 +11,3 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-from dbgen.exceptions import MissingImportError
-
-# Ensure boto3 is installed for the aws packages
-try:
-    import boto3  # noqa: F401
-except ModuleNotFoundError as exc:
-    raise MissingImportError(
-        "boto3 is not installed. to use the dbgen.providers.aws package you must install dbgen with the extras [boto3].\n"
-        "python -m pip install 'modelyst-dbgen[boto3]'"
-    ) from exc
-
-from dbgen.providers.aws.extract import S3FileExtractor, S3FileNameExtractor  # noqa: F401
